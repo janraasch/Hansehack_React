1c894054e626b4ab35579515510b3ed200718e4e
MSM\Nikodem.Grzonkowski <Nikodem.Grzonkowski@msm-success.de>
2018-09-06 16:41:58 +0200

    adding tr elements to table

--- src/SensorList.js
+++ src/SensorList.js
@@ -3,30 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class SensorList extends Component {
+
+    addTableRow(id, location, noiseInDb)
+    {
+        return (<tr><td>{id}</td><td>{location}</td><td>{noiseInDb}</td></tr>);
+    }
+
+    fetchAllTableRows(jsonResult)
+    {
+        return jsonResult.data.map((channel) => (
+            (<tr key={channel.id}><td>{channel.id}</td><td>{channel.location}</td><td>{channel.noise}</td></tr>)
+        ));
+    }
+
     render() {
+
+        const jsonData = {data: [{id: 1, location: "abcd", noise: 20}, {id: 2, location: "abcd", noise: 20}]};
+
         return (
             <div className="App">
-
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Listing all sensors</h1>
                 </header>
-                <table style={{width: 100 + '%'}}>
-                    <tr>
-                        <th>Device ID</th>
-                        <th>Street</th>
-                        <th>Noise in DB</th>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Hamburgerstraße 11</td>
-                        <td>10</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Geniner Straße 30</td>
-                        <td>20</td>
-                    </tr>
+                <table id="table" style={{width: 100 + '%'}}>
+                    <tbody>
+                      {this.fetchAllTableRows(jsonData)}
+                    </tbody>
                 </table>
             </div>
         );
