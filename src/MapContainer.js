1350b6d50bded9ca4f645f649bf9fa2b58a12fb3
APPDEV\AGPark <master_vad@hotmail.de>
2018-09-06 18:00:22 +0200

    Added GoogleMapsApi

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import GoogleMapsContainer from './Components/GoogleMapsContainer.js'
+
 class MapContainer extends Component {
   render() {
     return (
@@ -13,7 +15,9 @@ class MapContainer extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <GoogleMapsContainer/>
       </div>
+
     );
   }
 }
