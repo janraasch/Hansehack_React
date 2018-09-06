6f161d44f9b5af1a79820227be0ca87893b02204
Hannes Maack <hmaack@rocknruby.it>
2018-09-06 17:21:42 +0200

    fetch data from sensors and provide access to the container

--- src/App.js
+++ src/App.js
@@ -4,31 +4,53 @@ import SensorList from "./SensorList";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import MapContainer from './MapContainer';
 
-const channels = {
-  76: {
-    name: 'Wallstrasse 43',
-    lat: 45.1213,
-    lng: 51.1123
+const channels = [
+  {
+    id: 121,
+    token: 'UXPMPOC1TQQXBSAP'
+  }, {
+    id: 122,
+    token: 'KPW4D81CMQ9YZX1G'
+  }, {
+    id: 123,
+    token: '4RIMYW1C4I56I5FR'
   }
-}
+]
 const serverURI = 'http://hansehack.fh-luebeck.de'
-const token = 'X8J5RBSQADFV0DO2'
 
 class App extends Component {
   state = {
-    data: []
+    channelData: []
   }
 
-  // componentDidMount() {
-  //   axios.get(`${serverURI}/channels/76/feed.json?key=${token}`)
-  //   .then(res => {
-  //     const posts = res.data.data.children.map(obj => obj.data);
-  //     this.setState({ posts });
-  //   });
-  // }
+  componentDidMount() {
+    channels.map((channel) => {
+      axios.get(`${serverURI}/channels/${channel.id}/feeds.json?results=1&key=${channel.token}`)
+      .then(res => {
+        const { name, latitude, longitude } = res.data.channel
+        const feeds = res.data.feeds
+        
+        const { channelData } = this.state;
+        const lastLevel = feeds && feeds[0] ? feeds[0].field1 : null
+
+        const fetchedData = channelData.concat([{
+          name,
+          latitude,
+          longitude,
+          lastLevel: lastLevel
+        }]);
+        this.setState({ channelData: fetchedData });
+      });
+      return null;
+    })
+  }
 
 
   render() {
+    const { channelData } = this.state;
+    const feeds = channelData.map((channel) => {
+      return (<li>{channel.name}:{channel.lastLevel}</li>);
+    })
     return (
       <Router>
         <div>
@@ -41,8 +63,9 @@ class App extends Component {
             </li>
           </ul>
           <hr />
-          <Route path="/map" component={MapContainer} />
-          <Route path="/SensorList" component={SensorList} />
+          <Route path="/map" component={() => <MapContainer channelData={channelData}/>} />
+          <Route path="/SensorList" component={() => <SensorList channelData={channelData}/>} />
+          {channelData.length > 0 ? (<ul>{feeds}</ul>) : '... loading server data'}
         </div>
       </Router>
     );
