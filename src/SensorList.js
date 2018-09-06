1abdc2c0719b8d307aecdefb806a94f9170f6810
MSM\Nikodem.Grzonkowski <Nikodem.Grzonkowski@msm-success.de>
2018-09-06 15:17:53 +0200

    added dummy table view

--- src/SensorList.js
+++ src/SensorList.js
@@ -6,13 +6,30 @@ class SensorList extends Component {
     render() {
         return (
             <div className="App">
+
+
+
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">Listing all sensors</h1>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <table style={{width: 100 + '%'}}>
+                    <tr>
+                        <th>Device ID</th>
+                        <th>Street</th>
+                        <th>Noise in DB</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Hamburgerstraße 11</td>
+                        <td>10</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Geniner Straße 30</td>
+                        <td>20</td>
+                    </tr>
+                </table>
             </div>
         );
     }
