207ec34ebbf86cf1c8cf0bffc0895cfb6739cbc6
MSM\Nikodem.Grzonkowski <Nikodem.Grzonkowski@msm-success.de>
2018-09-06 15:10:24 +0200

    added browserRoute dependency

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SensorList from "./SensorList";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router>
+            <div>
+                <ul>
+
+                    <li>
+                        <Link to="/SensorList">SensorList</Link>
+                    </li>
+                </ul>
+
+                <hr />
+
+                <Route path="/SensorList" component={SensorList} />
+            </div>
+        </Router>
     );
   }
 }
