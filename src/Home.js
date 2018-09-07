187dbef6229c7b90c67250dd85fc1fead188ccda
Hannes Maack <hmaack@rocknruby.it>
2018-09-07 18:00:34 +0200

    some home page tweaks

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Well, Jumbotron, Button } from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
-import './Home.css'
+import './Home.css';
 
 const Home = () => (
   <div>
-    <Jumbotron className="hero">
+    <Jumbotron className="hero text-center">
       <h1>Sensoring</h1>
       <p className="shadowing">
         Sensoring is a system based on sound sensor arrays placed around the city that allows to gather data about sound intensity in wide area. Each array consists of 4 microphones that can sense the sound from 360 degrees. Collected data are sent via LoRaWAN to the cetral database where they are processed by server software.
       </p>
-      <p>
-      <Button bsStyle="primary">Learn more 😇</Button>
+      <p className='text-center'>
+      <LinkContainer to="/map">
+        <Button bsStyle="primary" bsSize="large">Learn more 😇</Button>
+      </LinkContainer>
       </p>
     </Jumbotron>
     <Well>
